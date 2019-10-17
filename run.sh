39aef4f6f0606c1b66a6e5104e8b887dbe4129d8
root <root@yq01-gpu-127-1-139-00.epc.baidu.com>
2019-10-17 12:59:51 +0000

    fix py3 and win bugs

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 export FLAGS_sync_nccl_allreduce=0
 export FLAGS_eager_delete_tensor_gb=1
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=2
 
 if [[ ! -d pretrain_model/bert ]]; then
     bash download_pretrain.sh bert
@@ -14,5 +14,5 @@ if [[ ! -d pretrain_model/ernie ]]; then
     bash download_pretrain.sh ernie
 fi
 
-python -u mtl_run.py
+python3.5 -u mtl_run.py
 
