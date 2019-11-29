89c0e1c13e4e55ff9157f3d3b93fb44cfea241f7
wangxiao <zko1021@foxmail.com>
2019-11-29 18:04:16 +0800

    fix imports

--- paddlepalm/__init__.py
+++ paddlepalm/__init__.py
@@ -1,2 +1,9 @@
 import downloader
-from _mtl_controller import Controller 
+from mtl_controller import Controller 
+
+
+del interface
+del task_instance
+del default_settings
+del utils
+del mtl_controller
