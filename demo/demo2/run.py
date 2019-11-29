c1762638366845cc858bdf408627f2b16e74ad5b
wangxiao <zko1021@foxmail.com>
2019-11-29 12:52:52 +0800

    update run.py of 3 demos

--- demo/demo2/run.py
+++ demo/demo2/run.py
@@ -2,7 +2,7 @@ import paddlepalm as palm
 
 if __name__ == '__main__':
     controller = palm.Controller('config.yaml', task_dir='tasks')
-    controller.load_pretrain('../../pretrain_model/ernie/params')
+    controller.load_pretrain('../../pretrain/ernie-en-uncased-large/params')
     controller.train()
 
     controller = palm.Controller(config='config.yaml', task_dir='tasks', for_train=False)
--- demo/demo3/run.py
+++ demo/demo3/run.py
@@ -2,6 +2,6 @@ import paddlepalm as palm
 
 if __name__ == '__main__':
     controller = palm.Controller('config.yaml', task_dir='tasks')
-    controller.load_pretrain('../../pretrain_model/ernie/params')
+    controller.load_pretrain('../../pretrain/ernie-en-uncased-large/params')
     controller.train()
 
