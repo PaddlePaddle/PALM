aeec65fa59b0402426e16715cafc5d8db7ca1988
wangxiao1021 <zko1021@foxmail.com>
2019-12-24 18:45:00 +0800

    update match.py

--- paddlepalm/task_paradigm/match.py
+++ paddlepalm/task_paradigm/match.py
@@ -20,6 +20,18 @@ import numpy as np
 import os
 import json
 
+
+def computeHingeLoss(pos, neg):
+    loss_part1 = fluid.layers.elementwise_sub(
+        fluid.layers.fill_constant_batch_size_like(
+            input=pos, shape=[-1, 1], value=self._margin, dtype='float32'), pos)
+    loss_part2 = fluid.layers.elementwise_add(loss_part1, neg)
+    loss_part3 = fluid.layers.elementwise_max(
+        fluid.layers.fill_constant_batch_size_like(
+            input=loss_part2, shape=[-1, 1], value=0.0, dtype='float32'), loss_part2)
+    return loss_part3
+
+
 class TaskParadigm(task_paradigm):
     '''
     matching
@@ -28,6 +40,8 @@ class TaskParadigm(task_paradigm):
         self._is_training = phase == 'train'
         self._hidden_size = backbone_config['hidden_size']
         self._batch_size = config['batch_size']
+
+        self._num_classes = config.get('num_classes', 2)
         if 'learning_strategy' in config:
             self._learning_strategy = config['learning_strategy']
         else:
@@ -51,6 +65,7 @@ class TaskParadigm(task_paradigm):
 
         self._pred_output_path = config.get('pred_output_path', None)
         self._preds = []
+        self._preds_logits = []
 
     
     @property
@@ -73,7 +88,8 @@ class TaskParadigm(task_paradigm):
             if self._learning_strategy=='paiwise':
                 return {"probs": [[-1, 1], 'float32']}
             else:
-                return {"logits": [[-1, 2], 'float32']}
+                return {"logits": [[-1, 2], 'float32'],
+                        "probs": [[-1, 2], 'float32']}
 
     def build(self, inputs, scope_name=""):
 
@@ -98,22 +114,23 @@ class TaskParadigm(task_paradigm):
         if self._learning_strategy == 'pointwise':
             logits = fluid.layers.fc(
                 input=cls_feats,
-                size=2,
-                act="softmax",
+                size=self._num_classes,
                 param_attr=fluid.ParamAttr(
                     name=scope_name+"cls_out_w",
                     initializer=self._param_initializer),
                 bias_attr=fluid.ParamAttr(
                     name=scope_name+"cls_out_b",
                     initializer=fluid.initializer.Constant(0.)))
+            probs = fluid.layers.softmax(logits)
             if self._is_training:
                 ce_loss = fluid.layers.cross_entropy(
-                    input=logits, label=labels)
+                    input=probs, label=labels)
                 loss = fluid.layers.mean(x=ce_loss)
+                return {'loss': loss}
             # for pred
             else:
-                return {'logits': logits}
-
+                return {'logits': logits,
+                        'probs': probs}
         # for pairwise
         elif self._learning_strategy == 'pairwise':
             pos_score = fluid.layers.fc(
@@ -141,30 +158,23 @@ class TaskParadigm(task_paradigm):
                         initializer=fluid.initializer.Constant(0.)))        
                 neg_score = fluid.layers.reshape(x=neg_score, shape=[-1, 1], inplace=True)
         
-                def computeHingeLoss(pos, neg):
-                    loss_part1 = fluid.layers.elementwise_sub(
-                        fluid.layers.fill_constant_batch_size_like(
-                            input=pos, shape=[-1, 1], value=self._margin, dtype='float32'), pos)
-                    loss_part2 = fluid.layers.elementwise_add(loss_part1, neg)
-                    loss_part3 = fluid.layers.elementwise_max(
-                        fluid.layers.fill_constant_batch_size_like(
-                            input=loss_part2, shape=[-1, 1], value=0.0, dtype='float32'), loss_part2)
-                    return loss_part3
-
                 loss = fluid.layers.mean(computeHingeLoss(pos_score, neg_score))
                 return {'loss': loss}
             # for pred
             else:
                 return {'probs': pos_score}
+        
+
 
     def postprocess(self, rt_outputs):
         if not self._is_training:
-            outputs = []
+            probs = []
+            logits = []
+            probs = rt_outputs['probs']
+            self._preds.extend(probs.tolist())
             if self._learning_strategy == 'pointwise':
-                outputs = rt_outputs['logits']
-            elif self._learning_strategy == 'pairwise':
-                outputs = rt_outputs['probs']
-            self._preds.extend(outputs.tolist())
+                logits = rt_outputs['logits']
+                self._preds_logits.extend(logits.tolist())
         
     def epoch_postprocess(self, post_inputs):
         # there is no post_inputs needed and not declared in epoch_inputs_attrs, hence no elements exist in post_inputs
@@ -175,7 +185,7 @@ class TaskParadigm(task_paradigm):
                 for i in range(len(self._preds)):
                     if self._learning_strategy == 'pointwise':
                         label = 0 if self._preds[i][0] > self._preds[i][1] else 1
-                        result = {'index': i, 'label': label, 'logits': self._preds[i]}
+                        result = {'index': i, 'label': label, 'logits': self._preds_logits[i], 'probs': self._preds[i]}
                     elif self._learning_strategy == 'pairwise':
                         label = 0 if self._preds[i][0] < 0.5 else 1
                         result = {'index': i, 'label': label, 'probs': self._preds[i][0]}
