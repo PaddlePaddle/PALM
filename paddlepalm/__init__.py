7a47bd0a061d0cdd3cb4ec7416ba8092ea203f7f
wangxiao1021 <zko1021@foxmail.com>
2020-02-07 22:28:36 +0800

    fix install

--- paddlepalm/__init__.py
+++ paddlepalm/__init__.py
@@ -1,6 +1,6 @@
 import downloader
 # from mtl_controller import Controller 
-import controller
+#import controller
 import optimizer
 import lr_sched
 import backbone
@@ -11,7 +11,7 @@ import head
 from trainer import Trainer
 from multihead_trainer import MultiHeadTrainer
 
-del interface
-del task_instance
-del default_settings
-del utils
+#del interface
+#del task_instance
+#del default_settings
+#del utils
